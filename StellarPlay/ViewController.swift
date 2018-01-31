4606ca4330676ea693249cab0e23ed49a6459b17
kuyawa <haxapp@gmail.com>
2018-01-31 11:26:47 -0400

    Assets

--- StellarPlay/ViewController.swift
+++ StellarPlay/ViewController.swift
@@ -18,15 +18,21 @@ class ViewController: NSViewController, NSTextDelegate  {
     var accountsController = TableAccounts()
     var assetsController   = TableAssets()
     
+    @IBOutlet weak var tabMain: NSTabView!
+    
+    // Main view
     @IBOutlet weak var labelAccounts : NSTextField!
     @IBOutlet weak var labelGenerate : NSTextField!
     @IBOutlet weak var labelLedger   : NSTextField!
+
+    // Tables
     @IBOutlet weak var tableAccounts : NSTableView!
+    @IBOutlet weak var tableAssets   : NSTableView!
     
+    // Generate address
     @IBOutlet weak var textAddress : NSTextField!
     @IBOutlet weak var textSecret  : NSTextField!
     
-    @IBOutlet weak var tabMain: NSTabView!
     
     @IBAction func onTabAccounts(_ sender: AnyObject) {
         tabMain.selectTabViewItem(at: 0)
@@ -57,32 +63,57 @@ class ViewController: NSViewController, NSTextDelegate  {
 
     func main() {
         loadAccounts()
-        showBalances(0) // First account
+        loadAssets(0) // First account
     }
     
     func loadAccounts(){
         app.storage.loadAccounts(app)
         accountsController.list           = app.storage.accounts
         accountsController.tableView      = tableAccounts
-        accountsController.tableSelection = showBalances
-        tableAccounts.delegate   = accountsController
-        tableAccounts.dataSource = accountsController
-        tableAccounts.target     = accountsController
+        accountsController.tableSelection = loadAssets
+        tableAccounts.delegate            = accountsController
+        tableAccounts.dataSource          = accountsController
+        tableAccounts.target              = accountsController
         //tableAccounts.doubleAction = #selector(onSelectedAccount(_:))
         tableAccounts.reloadData()
         selectedAccount = 0
     }
     
-    func showBalances(_ selected: Int) {
-        if selectedAccount < 0 { return }
+    func loadAssets(_ selected: Int) {
+        if selected < 0 || selected >= accountsController.list.count { return }
+        assetsLoading()
         let row = accountsController.list[selected]
         let net = row.net=="Test" ? StellarSDK.Horizon.Network.test : StellarSDK.Horizon.Network.live
         let account = StellarSDK.Account(row.key, net)
-        account.getInfo { info in
-            for balance in info.balances {
+        account.getBalances { balances in
+            var assets: [AssetData] = []
+            for balance in balances {
                 print(balance.assetCode, balance.balance, balance.assetType)
+                let asset = AssetData(symbol: balance.assetCode, issuer: balance.assetIssuer, amount: balance.balance)
+                assets.append(asset)
             }
+            DispatchQueue.main.async { self.showAssets(assets) }
         }
+    }
+
+    func assetsLoading() {
+        // Temp fix, rethink
+        var assets: [AssetData] = []
+        let asset = AssetData(symbol: "Loading...", issuer: "Wait while we fetch the server", amount: "")
+        assets.append(asset)
+        showAssets(assets)
+    }
+    
+    func showAssets(_ assets: [AssetData]) {
+        assetsController.list           = assets
+        assetsController.tableView      = tableAssets
+        //assetsController.tableSelection = ??
+        tableAssets.delegate            = assetsController
+        tableAssets.dataSource          = assetsController
+        tableAssets.target              = assetsController
+        //tableAassets.doubleAction = #selector(onSelectedAccount(_:))
+        tableAssets.reloadData()
+        //selectedAsset = 0
     }
     
     func generateKeypair() {
